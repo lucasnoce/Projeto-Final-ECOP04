88f5cb6f7256c14ed58ba995194b079f1f00f7d9
Lucas Noce <lucasnoce0@gmail.com>
2020-12-03 19:26:54 -0300

    Otimização da main

--- main_projeto.c
+++ main_projeto.c
@@ -7,6 +7,7 @@
 #include "keypad.h"
 #include "timer.h"
 #include "lcd.h"
+#include "adc.h"
 
 #define BUMBO 40
 #define CAIXA 48
@@ -22,10 +23,50 @@
 #define METRON 7
 #define LCD    6
 
+unsigned char flag = 0xC0;
+static unsigned int bpm = 120;
+unsigned int countMetron = 0;
+static unsigned char velocity = 0x50;
+unsigned int auxvel;
+
 void tocarSom(unsigned char key){
   serialSend(0x90); //note ON event; ch 1
   serialSend(key); //key
-  serialSend(0x50); //velocity
+  serialSend(velocity); //velocity
+  serialSend('\n');
+}
+
+void checarTimer(){
+  if (timerEnded() && bitTst(flag, METRON)){
+    timerReset(10000);
+    countMetron++;
+    if (countMetron >= (6000/bpm)){
+      bitClr(TRISB, 7);
+      bitSet(PORTB, 7);
+      countMetron = 0;
+    }
+    else if (countMetron == 20){
+      bitClr(TRISB, 7);
+      bitClr(PORTB, 7);
+    }
+  }
+}
+
+void checarLCD(){
+  if (!bitTst(flag, METRON) && bitTst(flag, LCD)){
+    bitClr(TRISB, 7);
+    bitClr(PORTB, 7);
+    print(-1);
+    bitClr(flag, LCD);
+  }
+  else if (bitTst(flag, METRON) && !bitTst(flag, LCD)){
+    print(bpm);
+    bitSet(flag, LCD);
+  }
+}
+
+void checarADC(){
+  velocity = adc_amostra() >> 3;
 }
 
 
@@ -35,84 +76,48 @@ void main(void){
   serialInit();
   lcdInit();
   
-  unsigned char flag = 0xC0;
-  static unsigned int bpm = 120;
-  unsigned int countMetron = 0;
   print(bpm);
-  
   unsigned int valorTeclado; //valor lido pelo telcado
-  unsigned char valorSerial;
-  float i;
+  unsigned char n = 0;
 
   for(;;){
-    if (timerEnded() && bitTst(flag, METRON)){
-      
-      timerReset(10000); //datasheet pic18f4520
-      countMetron++;
-      if (countMetron >= (6000/bpm)){
-        bitClr(TRISB, 7);
-        bitSet(PORTB, 7);
-        countMetron = 0;
-      }
-      else if (countMetron == 20){
-        bitClr(TRISB, 7);
-        bitClr(PORTB, 7);
-      }
+    switch (n){
+      case 0: checarTimer();
+              n = 1;
+              break;
+      case 1: checarLCD();
+              n = 2;
+              break;
+      case 2: checarADC();
+              n = 0;
+              break;
     }
-    
-    if (!bitTst(flag, METRON) && bitTst(flag, LCD)){
-      bitClr(TRISB, 7);
-      bitClr(PORTB, 7);
-      print(-1);
-      bitClr(flag, LCD);
-    }
-    else if (bitTst(flag, METRON) && !bitTst(flag, LCD)){
-      print(bpm);
-      bitSet(flag, LCD);
-    }
-    
     
     //leitura do teclado
     kpDebounce();
     valorTeclado = kpRead();
     switch (valorTeclado){
-      case 1:    valorSerial = 12; break;
-      case 2:    valorSerial = 7; break;
-      case 4:    valorSerial = 4; break;
-      case 8:    valorSerial = 1; break;
-      case 16:   valorSerial = 0; break;
-      case 32:   valorSerial = 8; break;
-      case 64:   valorSerial = 5; break;
-      case 128:  valorSerial = 2; break;
-      case 256:  valorSerial = 14; break;
-      case 512:  valorSerial = 9; break;
-      case 1024: valorSerial = 6; break;
-      case 2048: valorSerial = 3; break;
-      default:   valorSerial = 10; break;
-    }
-    if (valorSerial != 10){
-      switch (valorSerial){
-        case 1: tocarSom(BUMBO); break;
-        case 2: tocarSom(CAIXA); break;
-        case 3: tocarSom(TOM_1); break;
-        case 4: tocarSom(TOM_2); break;
-        case 5: tocarSom(HIH_F); break;
-        case 6: tocarSom(HIH_A); break;
-        case 7: tocarSom(RIDE); break;
-        case 8: tocarSom(CR_1); break;
-        case 9: tocarSom(CR_2); break;
-        case 12: if (bpm > 0) bpm--;
-                 else bpm = 1;
-                 bitClr(flag, LCD);
-                 break;
-        case  0: bitFlp(flag, METRON);
-                 break;
-        case 14: if (bpm < 300) bpm++;
-                 else bpm = 300;
-                 bitClr(flag, LCD);
-                 break;
-        default: break;
-      }
+      case 0:    break;                  //nenhuma tecla
+      case 8:    tocarSom(BUMBO); break; //tecla 1
+      case 128:  tocarSom(CAIXA); break; //tecla 2
+      case 2048: tocarSom(TOM_1); break; //tecla 3
+      case 4:    tocarSom(TOM_2); break; //tecla 4
+      case 64:   tocarSom(HIH_F); break; //tecla 5
+      case 1024: tocarSom(HIH_A); break; //tecla 6
+      case 2:    tocarSom(RIDE);  break; //tecla 7
+      case 32:   tocarSom(CR_1);  break; //tecla 8
+      case 512:  tocarSom(CR_2);  break; //tecla 9
+      case 1:   if (bpm > 0) bpm--;      //tecla *
+                else bpm = 1;
+                bitClr(flag, LCD);
+                break;
+      case 16:  bitFlp(flag, METRON);    //tecla 0
+                break;
+      case 256: if (bpm < 300) bpm++;    //tecla #
+                else bpm = 300;
+                bitClr(flag, LCD);
+                break;
+      default: break;
     }
   }
 }
