01dbd39e8061823b27a109f71163caf8f20495ce
Lucas Noce <lucasnoce0@gmail.com>
2020-12-09 22:48:58 -0300

    Adicionados os aruqivos compilados

---
+++ build/default/production/main_projeto.p1.d
@@ -0,0 +1,10 @@
+build/default/production/main_projeto.p1:  \
+main_projeto.c  \
+bits.h  \
+config.h  \
+io.h  \
+keypad.h  \
+serial.h  \
+timer.h  \
+lcd.h  \
+adc.h 
---
+++ build/default/production/serial.p1.d
@@ -0,0 +1,5 @@
+build/default/production/serial.p1:  \
+serial.c  \
+serial.h  \
+io.h  \
+bits.h 
